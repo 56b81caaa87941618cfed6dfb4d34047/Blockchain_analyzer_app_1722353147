09a34ea059689ef778f4cba3b3504cf89fa301a3
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-07-30 21:09:44 +0530

    Code generation

--- components/simple_footer_component_1722353151.js
+++ components/simple_footer_component_1722353151.js
@@ -30,12 +30,12 @@ Vue.component("simple_footer_component_1722353151", {
             </div>
         </nav>
         <footer id="footer-section" class="bg-white dark:bg-gray-900">
-            <div id="footer-container" class="max-w-screen-xl p-6 py-8 mx-auto lg:py-16 md:p-8 lg:p-10">
-                <hr id="footer-divider" class="my-8 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-10">
+            <div id="footer-container" class="max-w-screen-xl p-8 py-12 mx-auto lg:py-16">
+                <hr id="footer-divider" class="my-10 border-gray-200 sm:mx-auto dark:border-gray-700">
                 <div id="footer-content" class="text-center">
-                    <div class="flex justify-center">
-                        <a id="footer-logo-link" href="#" class="flex items-center mb-6 text-3xl font-bold text-purple-600 dark:text-purple-400">
-                            <img id="footer-logo" src="./images/logo.svg" class="h-8 mr-4 sm:h-10" alt="Landwind Logo" />
+                    <div class="flex justify-center mb-6">
+                        <a id="footer-logo-link" href="#" class="flex items-center text-3xl font-bold text-purple-600 dark:text-purple-400">
+                            <img id="footer-logo" src="./images/logo.svg" class="h-10 mr-4" alt="BlockSight Logo" />
                             BlockSight - Blockchain Analytics Suite
                         </a>
                     </div>
